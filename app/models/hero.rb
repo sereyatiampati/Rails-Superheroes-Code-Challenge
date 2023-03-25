41b5f4b821d90745785587abfee8b4f6412364b3
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-25 17:34:00 +0300

    added asscociations and validations

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,2 +1,7 @@
 class Hero < ApplicationRecord
+    has_many :hero_powers
+    has_many :powers, through: :hero_powers
+
+    validates :name, presence:true
+    validates :super_name, presence:true
 end
