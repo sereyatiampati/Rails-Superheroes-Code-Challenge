7479fe08b93903ca8b9011e8fb24f0d1e12c30bd
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-25 17:39:52 +0300

    added seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Seeding..'
+
+hero1=Hero.create(name: "Kamala Khan",super_name:"Ms. Marvel")
+hero2= Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+hero3=Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts 'Done seeding!'
