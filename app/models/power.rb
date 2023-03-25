7ad9add0c75941b10e6d9408836c5405c1a4343e
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-25 17:36:43 +0300

    added  associations and validations for Power attributes

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,3 +1,6 @@
 class Power < ApplicationRecord
-    validates :description, presence:true, length: {minimum:20}
+    has_many :hero_powers
+    has_many :heros, through: :hero_powers
+    validates :name, presence:true
+    validates :description, presence:true, length: { minimum:20 }
 end
